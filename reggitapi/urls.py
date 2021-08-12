6135542fc4c058ed455c08426404dfd77810d569
peter <peter.white@ampereanalysis.com>
2021-08-12 13:32:55 +0100

    changed doc schema

--- reggitapi/urls.py
+++ reggitapi/urls.py
@@ -27,6 +27,7 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
    openapi.Info(
       title="Reggit API",
+      default_version='v1',
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
