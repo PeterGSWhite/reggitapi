762bce90c6ee7fb24bb00796587a23035ce953af
peter <peter.white@ampereanalysis.com>
2021-08-12 13:12:24 +0100

    changed doc schema

--- reggitapi/urls.py
+++ reggitapi/urls.py
@@ -20,13 +20,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from rest_framework.documentation import include_docs_urls
 from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Reggit API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include('reggit.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^docs/', schema_view)
 ]
